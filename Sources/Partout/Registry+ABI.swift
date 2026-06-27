dd7c459dcb1ea0e72957ae4d79a8a39c3489be10
RoseContee <43502926+RoseContee@users.noreply.github.com>
2026-06-27 01:54:43 +0100

    fix 'OpenVPNConnection' is inaccessible due to 'internal' protection level

--- Sources/Partout/Registry+ABI.swift
+++ Sources/Partout/Registry+ABI.swift
@@ -4,10 +4,10 @@
 
 extension Registry {
 #if PARTOUT_OPENVPN
-    typealias OpenVPNConnection = _OpenVPNConnectionV3
+    public typealias OpenVPNConnection = _OpenVPNConnectionV3
 #endif
 #if PARTOUT_WIREGUARD
-    typealias WireGuardConnection = _WireGuardConnectionV2
+    public typealias WireGuardConnection = _WireGuardConnectionV2
 #endif
 
     private static let noCacheURL = URL(filePath: "")
